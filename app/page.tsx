3e8abe2c9743993ffa7a6d3d626ce536511441af
Jakub Gołos <kontaktjakub0@gmail.com>
2025-05-24 16:24:39 +0200

    aktualizacja kolorów cd

--- app/page.tsx
+++ app/page.tsx
@@ -121,7 +121,7 @@ function AgentVisualizer() {
     );
   }
   return (
-    <div className="h-[300px] w-full bg-[#f7f7f6]">
+    <div className="h-[300px] w-full">
       <BarVisualizer
         state={agentState}
         barCount={20}
