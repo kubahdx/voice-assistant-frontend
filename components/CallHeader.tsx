fe55f67b0d27064d7f1b6e365d8eab1528ed5001
Jakub Gołos <kontaktjakub0@gmail.com>
2025-05-25 19:11:14 +0200

    123123123

---
+++ components/CallHeader.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { useEffect, useState } from "react";
+
+export default function CallHeader() {
+  const [elapsedTime, setElapsedTime] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setElapsedTime((prevTime: number) => prevTime + 1);
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const formatTime = (timeInSeconds: number) => {
+    const minutes = Math.floor(timeInSeconds / 60)
+      .toString()
+      .padStart(2, "0");
+    const seconds = (timeInSeconds % 60).toString().padStart(2, "0");
+    return `${minutes}:${seconds}`;
+  };
+
+  return (
+    <div className="text-center py-4">
+      <div className="text-xl font-bold text-[#C3CB9C]">
+        Sonder {formatTime(elapsedTime)}
+      </div>
+      <div className="text-sm text-gray-500">by Jakub G.</div>
+    </div>
+  );
+} 
