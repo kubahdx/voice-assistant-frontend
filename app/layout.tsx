bdb5311ed565b5558ee7acf1f2cf39302440b645
Jakub Gołos <kontaktjakub0@gmail.com>
2025-05-25 16:08:11 +0200

    zmiana tła

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`h-full ${publicSans400.className}`}>
-      <body className="h-full">{children}</body>
+      <body className="h-full bg-[#F6F6F6]">{children}</body>
     </html>
   );
 }
